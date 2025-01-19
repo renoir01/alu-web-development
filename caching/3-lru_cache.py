283d78e9cb7d73788d1f0b4362eb4cc5f58c7083
renoir01 <r.kaze@alustudent.com>
2025-01-19 08:43:22 +0200

    caching  web

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python3
-"""
-LRU Caching module - implements LRU caching system
-"""
+"""LRU Caching module - implements LRU caching system"""
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """
-    LRUCache defines:
-      - LRU caching system
-      - Max number of items defined by BaseCaching.MAX_ITEMS
-    """
+    """LRUCache defines a LRU caching system"""
 
     def __init__(self):
-        """
-        Initialize the LRU Cache
-        """
+        """Initialize the LRU Cache"""
         super().__init__()
         self.usage_order = []
 
     def put(self, key, item):
         """
         Add an item in the cache using LRU algorithm
-        Args:
-            key: The key to add
-            item: The value to store
         """
         if key is not None and item is not None:
             if (len(self.cache_data) >= self.MAX_ITEMS and
                     key not in self.cache_data):
-                # Remove least recently used item
                 lru_key = self.usage_order.pop(0)
                 del self.cache_data[lru_key]
                 print(f"DISCARD: {lru_key}")
@@ -42,10 +30,6 @@ class LRUCache(BaseCaching):
     def get(self, key):
         """
         Retrieve an item from the cache
-        Args:
-            key: The key to look for
-        Returns:
-            The value associated with the key if it exists, None otherwise
         """
         if key is not None and key in self.cache_data:
             self.usage_order.remove(key)
