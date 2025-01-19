2ffb22878dbe3ea9e024a8384fcc51f051304c0b
renoir01 <r.kaze@alustudent.com>
2025-01-19 08:45:00 +0200

    caching  web

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,50 +1,35 @@
 #!/usr/bin/python3
-"""
-FIFO Caching module - implements FIFO caching system
-"""
+"""LIFO Caching module - implements LIFO caching system"""
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class FIFOCache(BaseCaching):
-    """
-    FIFOCache defines:
-      - FIFO caching system
-      - Max number of items defined by BaseCaching.MAX_ITEMS
-    """
+class LIFOCache(BaseCaching):
+    """LIFOCache defines a LIFO caching system"""
 
     def __init__(self):
-        """
-        Initialize the FIFO Cache
-        """
+        """Initialize the LIFO Cache"""
         super().__init__()
-        self.queue = []
+        self.stack = []
 
     def put(self, key, item):
         """
-        Add an item in the cache using FIFO algorithm
-        Args:
-            key: The key to add
-            item: The value to store
+        Add an item in the cache using LIFO algorithm
         """
         if key is not None and item is not None:
             if (len(self.cache_data) >= self.MAX_ITEMS and
                     key not in self.cache_data):
-                # Remove first item (FIFO)
-                first_key = self.queue.pop(0)
-                del self.cache_data[first_key]
-                print(f"DISCARD: {first_key}")
+                last_key = self.stack.pop()
+                del self.cache_data[last_key]
+                print(f"DISCARD: {last_key}")
 
-            if key not in self.cache_data:
-                self.queue.append(key)
+            if key in self.cache_data:
+                self.stack.remove(key)
+            self.stack.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
         """
         Retrieve an item from the cache
-        Args:
-            key: The key to look for
-        Returns:
-            The value associated with the key if it exists, None otherwise
         """
         if key is not None:
             return self.cache_data.get(key)
